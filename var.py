d0a2b69b4b3ce1ae63e1553c28eabbbfcad16fd7
Kattupuchi <107940225+Kattupuchi@users.noreply.github.com>
2022-07-18 12:13:03 -0700

    Update var.py

--- var.py
+++ var.py
@@ -5,5 +5,5 @@ from os import getenv
 class Var:
     API_ID = config("API_ID", default=6, cast=int)
     API_HASH = config("API_HASH", "8da8adb41f654ba374788eb88003c3c4")
-    BOT_TOKEN = config("BOT_TOKEN", "5581135260:AAFuQsBYVrMhe12x--x19OXn5nXQHk0oyKE")
-    SUDO = list(map(int, getenv("SUDO", "5599715194").split()))
+    BOT_TOKEN = config("BOT_TOKEN", "5234005906:AAFutd-Kx3L1_Os5C0125oO1Btp5AiPPth8")
+    SUDO = list(map(int, getenv("SUDO", "5587651146").split()))
